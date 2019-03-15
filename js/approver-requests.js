fd57de4cd8188c5a585591de86a3de008a06bef9
Harry Hernández Rivera <harry.hernandez@upr.edu>
2019-03-15 16:17:45 -0400

    Updated css and php links.

--- js/approver-requests.js
+++ js/approver-requests.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-	$("button").click(function(){
-		$.get("approver-view-client-profile.php", function(data, status){
-                        var profile = JSON.parse(data);
-                        $("#profile-email").text(profile.email);
+	$("#view-profile").click(function(){
+		$.get("../server/approver-view-client-profile.php", function(data, status){
+			var profile = JSON.parse(data);
+			$("#profile-email").text(profile.email);
+			$("#profile-mobile").text(profile.mobile);
+			$("#profile-work").text(profile.work);
+			$("#profile-company").text(profile.company);
+			$("#profile-address").text(profile.address);
+			$("#profile-url").text(profile.url);
 			console.log(profile);
 		});
 	});
--- server/approver-view-client-profile.php
+++ server/approver-view-client-profile.php
@@ -1,5 +1,5 @@
 <?php
-$profile = array("email"=>"pedro.rivera2019@pepsi.com", "mobile"=>"(787)565-3134", "work"=>"(787)739-1452", "company"=>"Pepsi","Address"=>"Cll 9, Cidra, PR 00739","url"=>"http://pepsi.com");
+$profile = array("email"=>"pedro.rivera2019@pepsi.com", "mobile"=>"(787)565-3134", "work"=>"(787)739-1452", "company"=>"Pepsi","address"=>"Cll 9, Cidra, PR 00739","url"=>"http://pepsi.com");
 
 echo json_encode($profile);
 ?>
