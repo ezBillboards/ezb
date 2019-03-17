366c8d62c8e0e34fef2cf75a16af382d7618d402
Harry Hernández Rivera <harry.hernandez@upr.edu>
2019-03-17 16:42:13 -0400

    Image information dynamically changes.

--- js/approver-requests.js
+++ js/approver-requests.js
@@ -1,4 +1,5 @@
 var requests;
+var currentRequestIndex;
 
 getRequests();
 
@@ -21,42 +22,60 @@ $(document).ready(function(){
 			console.log(profile);
 		});
 	});
+
+	$(".carousel-control-prev").click(function(){
+
+	});
+	$(".carousel-control-next").click(function(){
+		
+	});
 });
 
 function getRequests(){
 	$.get("../server/approver-requests.php", function(data, status){
 		requests = JSON.parse(data);
+		currentRequestIndex = 0;
 		for (var i = 0; i < requests.length; i++) {
 			var request = "<a id=\"request" + i + "\" class=\"list-group-item " + ((i == 0) ? "active":"") + "\">" +
-        					"<div class=\"row request-queue-info\">" +
-          						"<div class=\"col-lg-3\">" +
-            						"<img src=\"" + requests[i].artworkURL + "\" class=\"img-rounded request-queue-images\" alt=\"" + requests[i].artworkName + "\">" +
-          						"</div>" +
-          						"<div class=\"col-lg-9\">" +
-            						"<div class=\"row request-queue-info\">" +
-              							"<div class=\"col-lg-6\"  style=\"padding-right: 0.5%;\">" +
-                							"<h4 class=\"list-group-item-heading text-right\" style=\"font-weight:bold;\">Request ID: </h4>" +
-              							"</div>" +
-              							"<div class=\"col-lg-6 text-left\" style=\"padding-left: 0.5%;\">" +
-                							"<h4 class=\"list-group-item-heading checkbox-text\" style=\"font-weight:normal;\"> " + requests[i].id + "</h4>" +
-              							"</div>" +
-            						"</div>" +
-            						"<div class=\"row\">" +
-              							"<div class=\"col-lg-7\">" +
-                							"<p class=\"list-group-item-text\">" + requests[i].billboard + "</p>" +
-                							"<p class=\"list-group-item-text\">" + requests[i].firstName + " " + requests[i].lastName + "</p>" +
-              							"</div>" +
-              							"<div class=\"col-lg-5\">" +
-                							"<p class=\"list-group-item-text\">" + requests[i].displayPerCycle + "</p>" +
-                							"<p class=\"list-group-item-text\">" + requests[i].date + "</p>" +
-              							"</div>" +
-            						"</div>" +
-          						"</div>" +
-        					"</div>" +
-      					   "</a>";
-			$("#request-queue").append(request);	
+			"<div class=\"row request-queue-info\">" +
+			"<div class=\"col-lg-3\">" +
+			"<img src=\"" + requests[i].artworkURL + "\" class=\"img-rounded request-queue-images\" alt=\"" + requests[i].artworkName + "\">" +
+			"</div>" +
+			"<div class=\"col-lg-9\">" +
+			"<div class=\"row request-queue-info\">" +
+			"<div class=\"col-lg-6\"  style=\"padding-right: 0.5%;\">" +
+			"<h4 class=\"list-group-item-heading text-right\" style=\"font-weight:bold;\">Request ID: </h4>" +
+			"</div>" +
+			"<div class=\"col-lg-6 text-left\" style=\"padding-left: 0.5%;\">" +
+			"<h4 class=\"list-group-item-heading checkbox-text\" style=\"font-weight:normal;\"> " + requests[i].id + "</h4>" +
+			"</div>" +
+			"</div>" +
+			"<div class=\"row\">" +
+			"<div class=\"col-lg-7\">" +
+			"<p class=\"list-group-item-text\">" + requests[i].billboard + "</p>" +
+			"<p class=\"list-group-item-text\">" + requests[i].firstName + " " + requests[i].lastName + "</p>" +
+			"</div>" +
+			"<div class=\"col-lg-5\">" +
+			"<p class=\"list-group-item-text\">" + requests[i].displayPerCycle + "</p>" +
+			"<p class=\"list-group-item-text\">" + requests[i].date + "</p>" +
+			"</div>" +
+			"</div>" +
+			"</div>" +
+			"</div>" +
+			"</a>";
+
+			var requestImages = "<div class=\"item " + ((i == 0) ? "active":"") + "\">" +
+            						"<img src=\"" + requests[i].artworkURL + "\" alt=\"" + requests[i].artworkName + "\" style=\"width:100%;\">" +
+            					"</div>";
+			$("#request-queue").append(request);
+			$("#request-images").append(requestImages);	
 		}
-		
+
+		$("#download-btn").attr("href",requests[0].artworkURL);
+		$("#image-name").text("Image: " + requests[0].artworkName + "." + requests[0].extension);
+		$("#image-resolution").text("Resolution: " + requests[0].width + " X " + requests[0].height + " pixels");
+		$("#image-extension").text("Item Type: " + requests[0].extension + " File");
+		$("#image-size").text("Size: " + requests[0].size);
 		console.log(requests);
 	});
 }
