d28e7cb8a3b712edcf66cd9f337bab132196e908
root <root@ezb.uprm.edu>
2019-03-21 17:29:57 -0400

    approver request js

--- js/approver-processed-requests.js
+++ js/approver-processed-requests.js
@@ -1,6 +1,6 @@
 /*approver processes requests JavaScript*/
 var requests;
-var tab;
+var tab = 'Approved';
 getApprovedRequests();
 getDeniedRequests();
 
@@ -12,7 +12,7 @@ $(document).ready(function(){
   
   $("#mySearch").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#"+ tab +" tr").filter(function() {
+    $("#"+ tab +" tbody tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
