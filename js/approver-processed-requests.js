4ff3a44dfca28ecb00d2f94849d2f6de2739f878
feloespejuelo <felix.gonzalez3@upr.edu>
2019-03-21 20:09:55 -0400

    Added view profile modal view to approved tab...Testing

--- js/approver-processed-requests.js
+++ js/approver-processed-requests.js
@@ -17,6 +17,25 @@ $(document).ready(function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+  
+  $("#view-profile").click(function(){
+		$.get("../server/approver-view-client-profile.php", {id:requests[currentRequestIndex].id}, function(data, status){
+			var profile = JSON.parse(data);
+			$("#profile-name").text(profile.firstName + " " + profile.lastName);
+			$("#profile-email").text(profile.email);
+			$("#profile-mobile").text(profile.mobile);
+			$("#profile-work").text(profile.work);
+			$("#profile-company").text(profile.company);
+			$("#profile-address1").text(profile.address1);
+			$("#profile-address2").text(profile.address2);
+			$("#profile-city-state-zipcode").text(profile.city + ", " + profile.state + " " + profile.zipcode);
+			$("#profile-url").text(profile.url);
+			$("#profile-facebook").attr("href",profile.facebookURL);
+			$("#profile-instagram").attr("href",profile.instagramURL);
+			$("#profile-twitter").attr("href",profile.twitterURL);
+			console.log(profile);
+		});
+	});
 });
 
 
