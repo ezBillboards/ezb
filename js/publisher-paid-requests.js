40e6c8530bf51e3dd06055b93b42425fe5525a0b
feloespejuelo <felix.gonzalez3@upr.edu>
2019-03-25 18:08:29 -0400

    Publisher view, paid requests

---
+++ js/publisher-paid-requests.js
@@ -0,0 +1,57 @@
+/*Publisher paid requests JavaScript*/
+var requests;
+var currentRequestID;
+
+getPaidRequest();
+
+function getPaidRequests(){
+	$.get("../server/publisher-paid-requests.php",function(data,status){
+		requests = JSON.parse(data);
+		$("#paid-requests").empty();
+		for(var i = 0; i < requests.length; i++){
+			var request = "<tr>" +
+			"<td><img class=\"img-rounded processed-requests-images\"  src =\""+ requests[i].artworkURL + "\"></img> " +
+			"</td> " +
+			"<td>" +
+			"<div>Request ID:" + requests[i].id + "" +
+			"</div>" +
+			"<div>Request Date: " + requests[i].reqDate +"" +
+			"</div>" +
+			"<div>Requested by: " + requests[i].firstName + " " + requests[i].lastName +"" +
+			"</div>" +
+			"<div>Image: "  + requests[i].artworkName + "." + requests[i].extension + ""+
+			"</div>" +
+			"</td> " +
+			"<td>" +
+			"<div>Approved Date: " + requests[i].approveDate +"" +
+			"</div>" +
+			"<div>Approved by: " + requests[i].approverFirstName + " " + requests[i].approverLastName +"" +
+			"</div>" +
+			"</td> " +
+			"<td>" +
+			"<button type=\"button\" class=\"btn btn-info\" href= \"" + requests[i].artworkURL+"\" download =\"" requests[i].artworkName + "\".\"" + requests[i].extension "\"><span class=\"glyphicon glyphicon-user\"></span>Download Image" +
+			"</button>" +
+			"<button type=\"button\" onclick=\"publishRequest(this)\" id =\"" + requests[i].id +"\" class=\"btn btn-success\">Publish Request" +
+			"</button>" +
+			"</td> " +
+			"</tr>";
+			$("#paid-requests").append(request);
+			
+		}
+	});
+}
+
+function publishRequest(item){
+	currentRequestID = $(item).attr("id");
+	console.log(currentRequestID);
+	$.post("../server/publisher-paid-requests.php",{id:currentRequestID},
+		function(data, status){
+			if(status === "success"){
+				location.reload();
+			} else {
+				alert("Error publishing request!!");
+			}
+			console.log(data);
+			console.log(status);
+		});
+}
---
+++ server/publisher-paid-requests.php
@@ -0,0 +1,46 @@
+<?php
+
+define('DB_SERVER', 'ezb.uprm.edu');
+define('DB_USERNAME', 'ezb');
+define('DB_PASSWORD', 'ezb');
+define('DB_NAME', 'ezbillboards');
+ 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "CALL getProcessedRequest(5)";
+$result = mysqli_query($conn,$sql);
+$requests = array();
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        $request['id'] = $row['request_ID'];
+        $request['reqDate'] = $row['requestDate'];
+        $request['firstName'] = $row['firstName'];
+        $request['lastName'] = $row['lastName'];
+        $request['artworkName'] = $row['artworkName'];
+        $request['artworkURL'] = $row['artworkURL'];
+        $request['extension'] = $row['extension'];
+        $request['approveDate'] = $row['approveDate'];
+		$request['approverFirstName'] = $row['approverFirstName'];
+        $request['approverLastName'] = $row['approverLastName'];
+		/* $request['billboard_ID'] = $row['billboard_ID'];
+        $request['billboard'] = $row['billboardName'];
+		$request['displayPerCycle'] = $row['displayPerCycle'];
+		 request['width'] = $row['width'];
+        $request['height'] = $row['height'];
+        $request['size'] = $row['size'];*/
+        array_push($requests,$request);
+    }
+} else {
+    echo "No results";
+}
+
+mysqli_close($conn);
+
+echo json_encode($requests);
+
+?>
