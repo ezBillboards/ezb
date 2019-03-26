6944f993094ef75deda3e2c9cdbf553f4a07f989
feloespejuelo <felix.gonzalez3@upr.edu>
2019-03-26 08:26:02 -0400

    Publisher search bar debugging

--- js/publisher-history.js
+++ js/publisher-history.js
@@ -7,7 +7,7 @@ getHistory();
 $(document).ready(function(){
 	$("#mySearch").on("keyup", function() {
 		var value = $(this).val().toLowerCase();
-		$("#tbody tr").filter(function() {
+		$("#history tbody tr").filter(function() {
 			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 		});
 	});
