7ca203f80f49b11d0e064486f814634abbc8303e
Felix <felix.gonzalez3@upr.edu>
2019-03-28 12:21:57 -0400

    Login function test

--- js/user-home.js
+++ js/user-home.js
@@ -1,1 +1,17 @@
 /*user home view JavaScript*/
+
+Login();
+
+function Login(){
+	$.get("../servers/user-login.php",
+		function(data, status){
+			if(status === "success"){
+				location.reload();
+			} else {
+				alert("Error Login function!!");
+			}
+			console.log(data);
+			console.log(status);
+		});
+	
+}
---
+++ server/user-login.php
@@ -0,0 +1,31 @@
+<?php
+
+define('DB_SERVER', 'ezb.uprm.edu');
+define('DB_USERNAME', 'ezb');
+define('DB_PASSWORD', 'ezb');
+define('DB_NAME', 'ezbillboards');
+ 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "CALL getLoginInfo('example2@billboards.com')";
+$result = mysqli_query($conn,$sql);
+$requests = array();
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo $row['publisher_ID_OUT'];
+        /*array_push($requests,$request);*/
+    }
+} else {
+    echo "No results";
+}
+
+mysqli_close($conn);
+
+echo json_encode($requests);
+
+?>
