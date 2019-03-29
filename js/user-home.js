97ee1f477d717fdc0fe065337c43481e4274a283
Felix <felix.gonzalez3@upr.edu>
2019-03-29 12:42:38 -0400

    Unexpected token N in JSON debugging

--- js/user-home.js
+++ js/user-home.js
@@ -10,10 +10,7 @@ function Login(){
 		function(data, status){
 			console.log(Object.keys(data).length);
 			if(JSON.parse(data).length > 0){
-			console.log('Size > 0');
-			}
-			credentials = JSON.parse(data);
-			if(credentials.length > 0){
+				credentials = JSON.parse(data);
 				role = "USER";
 				console.log(credentials[0].id);
 				console.log(data);
@@ -23,21 +20,19 @@ function Login(){
 	$.get("../server/approver-credentials.php",
 		{emailAddress: 'example2@billboards.com'},
 		function(data, status){
-			credentials = JSON.parse(data);
-			if(credentials.length > 0){
+			if(JSON.parse(data).length > 0){
+				credentials = JSON.parse(data);
 				role = "APPROVER";
 				console.log(credentials[0].id);
 				console.log(data);
 				console.log(status);
-
 			}
-			
 		});
 	$.get("../server/publisher-credentials.php",
 		{emailAddress: 'example2@billboards.com'},
 		function(data, status){
-			credentials = JSON.parse(data);
-			if(credentials.length > 0){
+			if(JSON.parse(data).length > 0){
+				credentials = JSON.parse(data);
 				role = "PUBLISHER";
 				console.log(credentials[0].id);
 				console.log(data);
@@ -47,8 +42,8 @@ function Login(){
 	$.get("../server/admin-credentials.php",
 		{emailAddress: 'example2@billboards.com'},
 		function(data, status){
-			credentials = JSON.parse(data);
-			if(credentials.length > 0){
+			if(JSON.parse(data).length > 0){
+				credentials = JSON.parse(data);
 				role = "ADMIN";
 				console.log(credentials[0].id);
 				console.log(data);
@@ -56,6 +51,18 @@ function Login(){
 			}
 		});
 		
-	//User not found
-	//Error message
+	//IF USER IS FOUND 
+	//STEP 1: VERIFIED EMAIL??
+	//STEP 2: IF NOT VERIFIED: ENTER CODE OR RESEND CODE
+	//STEP 3: IF VERIFIED: SESSION VARIABLES AND HOME PAGE
+	
+	//IF APPROVER FOUND --->> APPROVER VIEW
+	
+	//IF PUBLISHER FOUND --->> PUBLISHER VIEW
+	
+	//IF ADMIN FOUND --->> ADMIN VIEW
+	
+	//USER NOT FOUND
+	//RELOAD MODAL WITH ERROR MESSAGE OR SOMETHING LIKE THAT....
+	//ERROR MESSAGE AND EMPTY PASSWORD INPUT BOX
 }
