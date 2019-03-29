36d4a5134a938449db5cc32b7da8bea62b420a33
Felix <felix.gonzalez3@upr.edu>
2019-03-29 13:14:00 -0400

    Login function testing

--- js/user-home.js
+++ js/user-home.js
@@ -1,6 +1,8 @@
 /*user home view JavaScript*/
 var credentials;
 var role;
+var profile_ID;
+var verifiedUser;
 
 Login();
 
@@ -12,7 +14,8 @@ function Login(){
 			if(JSON.parse(data).length > 0){
 				credentials = JSON.parse(data);
 				role = "USER";
-				console.log(credentials[0].id);
+				profile_ID = credentials[0].id;
+				verifiedUser = credentials[0].verified;
 				console.log(data);
 				console.log(status);
 			}
@@ -23,7 +26,7 @@ function Login(){
 			if(JSON.parse(data).length > 0){
 				credentials = JSON.parse(data);
 				role = "APPROVER";
-				console.log(credentials[0].id);
+				profile_ID = credentials[0].id;
 				console.log(data);
 				console.log(status);
 			}
@@ -33,8 +36,8 @@ function Login(){
 		function(data, status){
 			if(JSON.parse(data).length > 0){
 				credentials = JSON.parse(data);
+				profile_ID = credentials[0].id;
 				role = "PUBLISHER";
-				console.log(credentials[0].id);
 				console.log(data);
 				console.log(status);
 			}
@@ -45,24 +48,42 @@ function Login(){
 			if(JSON.parse(data).length > 0){
 				credentials = JSON.parse(data);
 				role = "ADMIN";
-				console.log(credentials[0].id);
+				profile_ID = credentials[0].id;
 				console.log(data);
 				console.log(status);
 			}
 		});
-		
-	//IF USER IS FOUND 
-	//STEP 1: VERIFIED EMAIL??
-	//STEP 2: IF NOT VERIFIED: ENTER CODE OR RESEND CODE
-	//STEP 3: IF VERIFIED: SESSION VARIABLES AND HOME PAGE
 	
-	//IF APPROVER FOUND --->> APPROVER VIEW
-	
-	//IF PUBLISHER FOUND --->> PUBLISHER VIEW
-	
-	//IF ADMIN FOUND --->> ADMIN VIEW
-	
-	//USER NOT FOUND
-	//RELOAD MODAL WITH ERROR MESSAGE OR SOMETHING LIKE THAT....
-	//ERROR MESSAGE AND EMPTY PASSWORD INPUT BOX
+	if (!role){
+		console.log('USER NOT FOUND');
+		//USER NOT FOUND
+		//RELOAD MODAL WITH ERROR MESSAGE OR SOMETHING LIKE THAT....
+		//ERROR MESSAGE AND EMPTY PASSWORD INPUT BOX
+	}
+	else if(role == "USER"){
+		//IF USER IS FOUND 
+		//STEP 1: VERIFIED EMAIL??
+		//STEP 2: IF NOT VERIFIED: ENTER CODE OR RESEND CODE
+		//STEP 3: IF VERIFIED: SESSION VARIABLES AND HOME PAGE
+		console.log('USER FOUND');
+		if (verified == 0){
+			console.log('USER NOT VERIFIED!')
+		}else{
+			console.log('USER VERIFIED!')
+		}
+	}
+	else if(role == "APPROVER"){
+		//SESSION VARIABLES
+		//IF APPROVER FOUND --->> APPROVER VIEW
+		window.location.href = "../approver/approver-requests.html";
+	}
+	else if(role == "PUBLISHER"){
+		//SESSION VARIABLES
+		//IF PUBLISHER FOUND --->> PUBLISHER VIEW
+		window.location.href = "../publisher/publisher-paid-requests.html";
+	}
+	else if(role == "ADMIN"){
+		//SESSION VARIABLES
+		//IF ADMIN FOUND --->> ADMIN VIEW
+	}
 }
