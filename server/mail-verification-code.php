9f85f2fb4a9c9fbf88f7899e2dcf9f3481c31eb7
Felix <felix.gonzalez3@upr.edu>
2019-04-01 11:26:16 -0400

    Send email function debugging

--- server/mail-verification-code.php
+++ server/mail-verification-code.php
@@ -9,5 +9,5 @@ $msg = "Your verification code is: " + $random + " ";
 $msg = wordwrap($msg,70);
 
 //Send email
-mail("felix.gonzalez3@upr.edu",Ezbillboards Verification code,$msg);
+mail("felix.gonzalez3@upr.edu","Ezbillboards Verification code",$msg);
 ?>
