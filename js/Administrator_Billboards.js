5feb09d39685c3e3515672448911d7e85316dc16
Felix <felix.gonzalez3@upr.edu>
2019-04-05 16:13:52 -0400

    Admin billboard HTML debugging

--- js/Administrator_Billboards.js
+++ js/Administrator_Billboards.js
@@ -1,7 +1,28 @@
+/*Administrator billboards JavaScript*/
+
 $(document).ready(function(){
     $(".nav-tabs a").click(function(){
       $(this).tab('show');
     });
+	
+	$('#btnaddpackage').click(function(){
+		console.log('btnaddpackage clicked!');
+		var newPackage = "<tr>" +
+		"<td>1 Day</td>" +
+		"<td>Every 4 min.</td>" +
+		"<td>$15.45</td>" +
+		"<td>" +
+		"<div class=\"column\">" +
+		"<div class=\"row\">" +
+		"<div class=\"col-lg-6\">" +
+		"<a href=""><span class=\"glyphicon glyphicon-pencil\"><br></span></a></div>" +
+		"<div class=\"col-lg-6\";">" +
+		"<a href=""><span class=\"glyphicon glyphicon-trash\"><br></span></a>" +
+		"</div>" +
+		"</div>" +
+		"</td>";
+		$("#add-package").append(newPackage);
+	});
   });
   
   // Add the following code if you want the name of the file appear on select
