10afbf78f35c31a422ed9c09d16c33ac7347d62d
Felix <felix.gonzalez3@upr.edu>
2019-04-05 23:16:25 -0400

    CSS debugging for file input

--- js/Administrator_Billboards.js
+++ js/Administrator_Billboards.js
@@ -25,11 +25,6 @@ $(document).ready(function(){
 	});
   });
   
-  // Add the following code if you want the name of the file appear on select
-  $(".custom-file-input").on("change", function() {
-    var fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-  });
   
   (function ($) {
     $('.spinner .btn:first-of-type').on('click', function() {
