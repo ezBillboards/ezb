9423dd8993c1b3398a08c1bf68e7b24987b8d066
Felix <felix.gonzalez3@upr.edu>
2019-04-09 23:10:25 -0400

    Post package and regulation testing

--- server/administrator-add-billboard.php
+++ server/administrator-add-billboard.php
@@ -30,7 +30,7 @@ $packages = $_POST['packages'];
 $regulations = $_POST['regulations'];
 $billboardID;
 
-/*$sql = "CALL postBillboard('$name','$description','$url',$width,$height,$latitude,$longitude,$minwidth,$maxwidth,$minheight,$maxheight,$readtime,$impressions,$traffic,$cycle)";
+$sql = "CALL postBillboard('$name','$description','$url',$width,$height,$latitude,$longitude,$minwidth,$maxwidth,$minheight,$maxheight,$readtime,$impressions,$traffic,$cycle)";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 	while($row = mysqli_fetch_assoc($result)) {
@@ -39,15 +39,38 @@ if (mysqli_num_rows($result) > 0) {
 	echo "New record created successfully. Last inserted ID is: " . $billboardID;
 } else {
 	echo "Error updating record: " . mysqli_error($conn);
-}*/
+}
+mysqli_close($conn);
 
 for ($x = 0; $x < count($packages); $x=$x+3) {
-    echo $packages[$x] . " " .$packages[$x + 1] . " " . $packages[$x + 2];
+	$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+	if($conn === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+	$sql = "CALL postPackage($billboardID,$packages[$x],$packages[$x + 1],$packages[$x + 2])";
+	if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+	} else {
+			echo "Error updating record: " . mysqli_error($conn);
+	}
+	mysqli_close($conn);	
 }
 
 for ($x = 0; $x < count($regulations); $x++) {
-    echo $regulations[$x];
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+	if($conn === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+	$sql = "CALL postRegulation($billboardID,'$regulations[$x]')";
+	if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+	} else {
+		echo "Error updating record: " . mysqli_error($conn);
+	}
+	mysqli_close($conn);
 }
 
-mysqli_close($conn);
+
 ?>
