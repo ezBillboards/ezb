02b03971c0f4c0219e0f76839d9028ae5e819bcb
Felix <felix.gonzalez3@upr.edu>
2019-04-09 22:11:20 -0400

    Administrator html, css, js and php. This is Elvin btw

---
+++ js/administrator-logs.js
@@ -0,0 +1,87 @@
+var requests;
+var currentRequestIndex;
+
+$(document).ready(function(){
+
+    $("#mySearchEmail").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$("tbody tr .emailCol").filter(function() {
+			$(this).parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	}); 
+
+
+  $("#mySearchAction").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("tbody tr .actionCol").filter(function() {
+                        $(this).parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+        });
+
+	$(function() {
+
+    		var start = moment().startOf('month');
+    		var end = moment().endOf('month');
+
+    		function updateDate(start, end) {
+			var startStr = start.format('YYYY-MM-DD');
+			var endStr = end.format('YYYY-MM-DD');
+			console.log(startStr);
+			console.log(endStr);
+			if(startStr == "Invalid date" && endStr == "Invalid date"){
+				$("#reportrange span").html("All");
+			} else{
+	        		$("#reportrange span").html(startStr + " to " + endStr);
+			}
+			getLogs(startStr, endStr);
+    		}
+
+    		$('#reportrange').daterangepicker({
+        		startDate: start,
+        		endDate: end,
+        		ranges: {
+           			'Today': [moment(), moment()],
+           			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           			'This Month': [moment().startOf('month'), moment().endOf('month')],
+           			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+	   			'All': 'All'
+        		}
+    		}, updateDate);
+
+    		updateDate(start, end);
+
+	});
+});
+
+
+function getLogs(startStr, endStr){
+	$("#logs").empty();
+	$.get("../server/administrator-logs.php",
+		{
+			start: startStr,
+			end: endStr
+		}, 
+		function(data, status){
+		console.log(data);
+		log = JSON.parse(data);
+		for (var i = 0; i < log.length; i++) {
+
+		var logs = "<tr>" +
+			"<td>" +log[i].date +
+			"</td>" +
+			"<td>" + log[i].timeStamp +
+			"</td>" +
+			"<td class = \"emailCol\"> " + log[i].email +
+			"</td>" +
+			"<td class = \"actionCol\"> " + log[i].action +
+			"</td>" +
+		        "<td><h5>" + log[i].detailedAction +
+                        "</h5></td>" 
+			"</tr>";
+
+			$("#logs").append(logs);
+		}
+	});
+}
