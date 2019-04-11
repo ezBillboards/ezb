d280643339aa058b104110eece4640674d113ad7
Felix <felix.gonzalez3@upr.edu>
2019-04-11 17:59:28 -0400

    Accounts view edited. BTW this is Harry.

--- js/administrator-accounts.js
+++ js/administrator-accounts.js
@@ -20,6 +20,34 @@ $(document).ready(function(){
       }
     });
 
+    $("#searchUsers").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody#user-accounts tr").filter(function() {
+	        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    	});
+    });
+
+    $("#searchApprovers").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody#approver-accounts tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    $("#searchPublishers").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody#publisher-accounts tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    $("#searchAdministrators").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody#administrator-accounts tr").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
     $("#add-account").click(function(){
 	$.post("../server/administrator-add-account.php",
 	{
@@ -32,6 +60,7 @@ $(document).ready(function(){
 		office: $("#office").val(),
 		role: $("#role").val()	
 	}, function(data,status){
+		validate();
 		console.log(data);	
       });
     });
@@ -79,29 +108,29 @@ function getUserAccounts(){
                 var account = "";
                 for(var i = 0; i < accounts.length; i++){
 			account += "<tr>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div class=\"row\">" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<div class=\"row text-center\" style=\"display: flex;align-items: center;\">" +
                                 "<div class=\"col-lg-3\" style=\"padding-right: 0;\">" +
-                                        "<span class=\"glyphicon glyphicon-user\"></span>" +
+                                        "<span class=\"glyphicon glyphicon-user\" style=\"font-size: 40px;\"></span>" +
                                 "</div>" +
-                                "<div class=\"col-lg-6 text-left\" style=\"padding-left: 0;\">" +
-                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5>" + accounts[i].email + "</h5>" +
+                                "<div class=\"col-lg-9 text-left\" style=\"padding-left: 0;\">" +
+                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5 style=\"color:gray;\"><i>" + accounts[i].email + "</i></h5>" +
                                 "</div>" +
                         "</div></td>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div>Work Phone: " + accounts[i].workPhone +
-                        "</div>" +
-                        "<div>Mobile Phone: " + accounts[i].mobilePhone +
-                        "</div>" +
-                        "<div>Company Name: " + accounts[i].company +
-                        "</div>" +
-                        "<div>Address: " + accounts[i].address1 + " " + accounts[i].address2 + " " + accounts[i].city + " " + accounts[i].state + " " + accounts[i].zip +
-                        "</div>" +
-                        "<div>URL: " + accounts[i].url +
-                        "</div>" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;vertical-align: middle;\">" +
+			"<p class=\text-center\"><b>Address: </b>" + accounts[i].address1 + " " + accounts[i].address2 + " " + accounts[i].city + " " + accounts[i].state + " " + accounts[i].zip +
+                        "</p>" +
+                        "<p class=\text-center\"><b>Work Phone: </b>" + accounts[i].workPhone +
+                        "</p>" +
+                        "<p class=\text-center\"><b>Mobile Phone: </b>" + accounts[i].mobilePhone +
+                        "</p>" +
+                        "<p class=\text-center\"><b>Company Name: </b>" + accounts[i].company +
+                        "</p>" +
+                        "<p class=\text-center\"><b>URL: </b>" + accounts[i].url +
+                        "</p>" +
                         "</td> " +
                         "<td class=\"text-center\" style=\"vertical-align: middle;width: 33.33%;\">" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\" style=\"font-size: 35px;\"></span>" +
 			"</td>" +
                         "</tr>";
                 }
@@ -117,26 +146,26 @@ function getApproverAccounts(){
                 var account = "";
                 for(var i = 0; i < accounts.length; i++){
 			account += "<tr>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div class=\"row\">" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<div class=\"row\"  style=\"display: flex;align-items: center;\">" +
                                 "<div class=\"col-lg-3\" style=\"padding-right: 0;\">" +
-                                        "<span class=\"glyphicon glyphicon-user\"></span>" +
+                                        "<span class=\"glyphicon glyphicon-user\" style=\"font-size: 40px;\"></span>" +
                                 "</div>" +
                                 "<div class=\"col-lg-6 text-left\" style=\"padding-left: 0;\">" +
-                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5>" + accounts[i].email + "</h5>" +
+                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5 style=\"color:gray;\"><i>" + accounts[i].email + "</i></h5>" +
                                 "</div>" +
                         "</div></td>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div>Work Phone: " + accounts[i].workPhone +
-                        "</div>" +
-                        "<div>Mobile Phone: " + accounts[i].mobilePhone +
-                        "</div>" +
-                        "<div>Office Number: " + accounts[i].office +
-                        "</div>" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;vertical-align: middle;\">" +
+                        "<p class=\"text-center\"><b>Work Phone: </b>" + accounts[i].workPhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Mobile Phone: </b>" + accounts[i].mobilePhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Office Number: </b>" + accounts[i].office +
+                        "</p>" +
                         "</td> " +
                         "<td class=\"text-center\" style=\"vertical-align: middle;width: 33.33%;\">" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\"></span>" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\" style=\"font-size: 35px;padding-right:5%;\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\" style=\"font-size: 35px;padding-left:5%;\"></span>" +
                         "</td>" +
                         "</tr>";
 
@@ -153,26 +182,26 @@ function getPublisherAccounts(){
                 var account = "";
                 for(var i = 0; i < accounts.length; i++){
                         account += "<tr>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div class=\"row\">" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<div class=\"row\" style=\"display: flex;align-items: center;\">" +
                                 "<div class=\"col-lg-3\" style=\"padding-right: 0;\">" +
-                                        "<span class=\"glyphicon glyphicon-user\"></span>" +
+                                        "<span class=\"glyphicon glyphicon-user\" style=\"font-size: 40px;\"></span>" +
                                 "</div>" +
                                 "<div class=\"col-lg-6 text-left\" style=\"padding-left: 0;\">" +
-                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5>" + accounts[i].email + "</h5>" +
+                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5 style=\"color:gray;\"><i>" + accounts[i].email + "</i></h5>" +
                                 "</div>" +
                         "</div></td>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div>Work Phone: " + accounts[i].workPhone +
-                        "</div>" +
-                        "<div>Mobile Phone: " + accounts[i].mobilePhone +
-                        "</div>" +
-                        "<div>Office Number: " + accounts[i].office +
-                        "</div>" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<p class=\"text-center\"><b>Work Phone: </b>" + accounts[i].workPhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Mobile Phone: </b>" + accounts[i].mobilePhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Office Number: </b>" + accounts[i].office +
+                        "</p>" +
                         "</td> " +
                         "<td class=\"text-center\" style=\"vertical-align: middle;width: 33.33%;\">" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\"></span>" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\" style=\"font-size: 35px;padding-right:5%;\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\" style=\"font-size: 35px;padding-left:5%;\"></span>" +
                         "</td>" +
                         "</tr>";
 
@@ -180,6 +209,81 @@ function getPublisherAccounts(){
                 $("#publisher-accounts").append(account);
         });
 }
+
+
+function validate(){
+  var firstName = document.getElementById('firstName').value;
+  var firstNameRGEX = /^[a-zA-Z ]{2,30}$/;
+  var firstNameResult = firstNameRGEX.test(firstName);
+  var lastName = document.getElementById('lastName').value;
+  var lastNameRGEX = /^[a-zA-Z ]{2,30}$/;
+  var lastNameResult = lastNameRGEX.test(lastName);
+  var email = document.getElementById('email').value;
+  var emailRGEX = /^(.+)@(.+)$/;
+  var emailResult = emailRGEX.test(email);
+  var wPhoneNumber = document.getElementById('workPhone').value;  
+  var wPhoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+  var wPhoneResult = wPhoneRGEX.test(wPhoneNumber);
+  var mPhoneNumber = document.getElementById('mobilePhone').value; 
+  var mPhoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+  var mPhoneResult = mPhoneRGEX.test(mPhoneNumber);
+  var office = document.getElementById('office').value;
+  var officeRGEX = /[\d -]+/;
+  var officeResult = officeRGEX.test(office);
+
+console.log('First Name = '+ firstName);
+console.log('Last Name = '+ lastName);
+console.log('Email = '+ email);
+console.log('Mobile Phone = '+ mPhoneNumber);
+console.log('Work Phone = '+ wPhoneNumber);
+console.log('Office Number = '+ office);
+
+
+
+if(firstNameResult == false)
+{
+alert('Please enter a valid First Name');
+return false;
+}
+
+if(lastNameResult == false)
+{
+alert('Please enter a valid Second Name');
+return false;
+}
+
+
+if(emailResult == false)
+{
+alert('Please enter a valid Email');
+return false;
+}
+
+
+if(wPhoneResult == false)
+{
+alert('Please enter a valid work number');
+return false;
+}
+
+
+if(mPhoneResult == false)
+{
+alert('Please enter a valid phone number');
+return false;
+}
+
+if(officeResult == false)
+{
+alert('Please enter a valid office');
+return false;
+}
+
+
+
+}
+
+
 
 function getAdministratorAccounts(){
         $.get("../server/administrator-non-user-accounts.php",{id:4}, function(data,status){
@@ -189,26 +293,26 @@ function getAdministratorAccounts(){
                 var account = "";
                 for(var i = 0; i < accounts.length; i++){
                         account += "<tr>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div class=\"row\">" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<div class=\"row\" style=\"display: flex;align-items: center;\">" +
                                 "<div class=\"col-lg-3\" style=\"padding-right: 0;\">" +
-                                        "<span class=\"glyphicon glyphicon-user\"></span>" +
+                                        "<span class=\"glyphicon glyphicon-user\" style=\"font-size: 40px;\"></span>" +
                                 "</div>" +
                                 "<div class=\"col-lg-6 text-left\" style=\"padding-left: 0;\">" +
-                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5>" + accounts[i].email + "</h5>" +
+                                        "<h4>" + accounts[i].firstName + " " + accounts[i].lastName + "</h4><h5 style=\"color:gray;\"><i>" + accounts[i].email + "</i></h5>" +
                                 "</div>" +
                         "</div></td>" +
-                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;\">" +
-                        "<div>Work Phone: " + accounts[i].workPhone +
-                        "</div>" +
-                        "<div>Mobile Phone: " + accounts[i].mobilePhone +
-                        "</div>" +
-                        "<div>Office Number: " + accounts[i].office +
-                        "</div>" +
+                        "<td class=\"text-center\" style=\"width: 33.33%;text-align: center;vertical-align: middle;\">" +
+                        "<p class=\"text-center\"><b>Work Phone: </b>" + accounts[i].workPhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Mobile Phone: </b>" + accounts[i].mobilePhone +
+                        "</p>" +
+                        "<p class=\"text-center\"><b>Office Number: </b>" + accounts[i].office +
+                        "</p>" +
                         "</td> " +
                         "<td class=\"text-center\" style=\"vertical-align: middle;width: 33.33%;\">" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\"></span>" +
-                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-pencil edit-account\" data-toggle=\"modal\" data-target=\"#EditModal\" style=\"font-size: 35px;padding-right:5%;\"></span>" +
+                                "<span id=\"" + i + "\" class=\"glyphicon glyphicon-trash delete-account\" style=\"font-size: 35px;padding-left:5%;\"></span>" +
                         "</td>" +
                         "</tr>";
 
