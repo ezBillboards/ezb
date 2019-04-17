240f08a34dbaa337c762bc5ef70358ee539fd477
Felix <felix.gonzalez3@upr.edu>
2019-04-16 23:01:22 -0400

    Session variables in approver and publisher

---
+++ js/approver.js
@@ -0,0 +1,33 @@
+
+session();
+
+$(document).ready(function(){
+	$("#btnlogout").click(function(){
+		console.log('btnlogout clicked!!');
+		sessionStorage.removeItem('ID');
+		sessionStorage.removeItem('email');
+		sessionStorage.removeItem('role');
+		Session();
+		window.location.href = "../user/home.html";
+	});
+});
+
+function session(){
+	if (sessionStorage.getItem('ID') !== null){
+		if(sessionStorage.getItem('role')!== 2){
+			if(sessionStorage.getItem('role') == 1){
+				window.location.href = "../user/home.html";
+			}
+			else if(sessionStorage.getItem('role') == 3){
+				window.location.href = "../publisher/paid-requests.html";
+			}
+			else{
+				window.location.href = "../administrator/settings.html";
+			}
+		}else{
+			document.getElementById('profile-email').value = sessionStorage.getItem('email');
+		}
+	}else{
+		window.location.href = "../user/home.html";
+	}
+}
