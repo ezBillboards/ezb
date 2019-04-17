0cfd6755be3969699f74ea025d6551c33dd442a5
Felix <felix.gonzalez3@upr.edu>
2019-04-17 01:41:21 -0400

    mail server send to real email verification code and forgot password

--- server/mail-verification-code.php
+++ server/mail-verification-code.php
@@ -3,16 +3,17 @@ date_default_timezone_set('Etc/UTC');
 require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 $mail = new PHPMailer; 
 $config = parse_ini_file('../../config.ini');
-
+	$emailAddress = $_POST['emailAddress'];
+	$random = $_POST['random'];
+	
 	$mail->isSMTP();
     $mail->Host = $config['IP'];
     $mail->Port = $config['PORT'];
     $mail->SMTPAuth = false;
 	
 	$mail->setFrom('ezbillboards@upr.edu', 'EZBillboards');
-	$mail->addAddress('felix.gonzalez3@upr.edu');
-	//$email = $_POST['email'];
-	$random = $_POST['random'];
+	$mail->addAddress($emailAddress);
+	
 	//The message
     $mail->Subject = 'Ezbillboards Verification Code';
     $mail->Body    = 'Your verification code for your recently created account is: ' .  $random;
