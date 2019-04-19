12a0b15ed53dd63c59af19e49d29148ed0b215e5
Harry Hernandez <harry.hernandez@upr.edu>
2019-04-19 10:58:12 -0400

    Made approver ID to decision.php

--- js/approver-requests.js
+++ js/approver-requests.js
@@ -118,6 +118,7 @@ $(document).ready(function(){
 		$.post("../server/approver-decision.php",
 		{
 			id:requests[currentRequestIndex].id,
+			approverID: sessionStorage.getItem('ID'),
 			status:decision,
 			comments:$("#comment").val()
 		},
