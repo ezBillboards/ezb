6d3f8d93903b85a7ee5d39eeab36fa3b31eadff1
Felix <felix.gonzalez3@upr.edu>
2019-04-27 21:38:35 -0400

    Add schedule and encryption js file

---
+++ server/add-schedule.php
@@ -0,0 +1,30 @@
+<?php
+
+date_default_timezone_set('Etc/UTC');
+require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+$config = parse_ini_file('../../config.ini');
+
+define('DB_SERVER', $config['DB_SERVER']);
+define('DB_USERNAME', $config['DB_USERNAME']);
+define('DB_PASSWORD', $config['DB_PASSWORD']);
+define('DB_NAME', $config['DB_NAME']);
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "CALL putNextSchedule()";
+
+if (mysqli_query($conn, $sql)) {
+	echo "Record updated successfully";
+	mysqli_close($conn);
+	
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+	mysqli_close($conn);
+}
+
+?>
