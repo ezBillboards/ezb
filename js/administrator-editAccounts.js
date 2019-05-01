2a00811f41f9a82a57074d998800cab88c5f002f
Felix <felix.gonzalez3@upr.edu>
2019-05-01 18:42:34 -0400

    Encryption session variables debugging

--- js/administrator-editAccounts.js
+++ js/administrator-editAccounts.js
@@ -9,23 +9,21 @@ $(document).ready(function(){
 		$.get("../server/user-account.php",
 			{id:decrypt(sessionStorage.getItem('ID'))},
 			function(data, status){
-
 				var info = JSON.parse(data);
-					console.log(info);
-					$("#firstName").val(info.firstName);
-					$("#lastName").val(info.lastName);
-					$("#email").val(info.email);
-					$("#mobilePhone").val(info.mobilePhone);
-					$("#workPhone").val(info.workPhone);
-					$("#company").val(info.company);
-					$("#address1").val(info.address1);
-					$("#address2").val(info.address2);
-					$("#state").val(info.state);
-					$("#city").val(info.city);
-					$("#zip").val(info.zip);
-
+				console.log(info);
+				$("#firstName").val(decrypt(info.firstName));
+				$("#lastName").val(decrypt(info.lastName));
+				$("#email").val(decrypt(info.email));
+				$("#mobilePhone").val(decrypt(info.mobilePhone));
+				$("#workPhone").val(decrypt(info.workPhone));
+				$("#company").val(decrypt(info.company));
+				$("#address1").val(decrypt(info.address1));
+				$("#address2").val(decrypt(info.address2));
+				$("#state").val(decrypt(info.state));
+				$("#city").val(decrypt(info.city));
+				$("#zip").val(decrypt(info.zip));
 			});
-	},100);
+	},50);
 	
 
 	$("#changePasswd").click(function(){
