1c1f42bbf445c6af907ddbd34c2e0c3c7d3638e2
Felix <felix.gonzalez3@upr.edu>
2019-05-02 00:56:54 -0400

    Register function and encryption;

--- js/user-home.js
+++ js/user-home.js
@@ -342,11 +342,12 @@ function Register(email_IN,firstName_IN,lastName_IN,mobilePhone_IN,password_IN,r
 						profile_ID = data;
 					}
 					setTimeout(function(){
+						sessionStorage.setItem('role', role);
 						sessionStorage.setItem('role', encrypt(role));
 						sessionStorage.setItem('email', encrypt(email_IN));
 						sessionStorage.setItem('verificationCode', encrypt(random_IN));
 						sendVerificationCode();
-					},50);
+					},200);
 					
 					
 					$('#registerModal').modal('hide');
