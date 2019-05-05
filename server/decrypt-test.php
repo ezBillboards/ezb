5d1dd4573e144082a91a87378bc0557e9bf44332
Felix <felix.gonzalez3@upr.edu>
2019-05-05 00:15:19 -0400

    decrypt test

--- server/decrypt-test.php
+++ server/decrypt-test.php
@@ -33,7 +33,7 @@ function cryptoJsAesDecrypt($passphrase, $jsonString){
 	echo $data;
     return json_decode($data, true);
 }
-cryptoJsAesDecrypt('hhrjo40OH0XXHZ9ygwQ9',json_encode(['4d4a69f20c3a63ef0efe489fea7703d1b21f9eadbcfe55c9acc9cb44a335e3666ht0KGjfiM0z7ySC8J0dgg==']),{format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8));
+cryptoJsAesDecrypt('hhrjo40OH0XXHZ9ygwQ9',json_encode(['4d4a69f20c3a63ef0efe489fea7703d1b21f9eadbcfe55c9acc9cb44a335e3666ht0KGjfiM0z7ySC8J0dgg==']),{format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8);
 /**
 * Encrypt value to a cryptojs compatiable json encoding string
 *
