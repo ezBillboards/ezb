8856c2d307bfc1a15b78d81c45238793f24fd5fe
Felix <felix.gonzalez3@upr.edu>
2019-05-04 23:51:58 -0400

    decrypt test

--- server/decrypt-test.php
+++ server/decrypt-test.php
@@ -13,7 +13,7 @@
 * @param mixed $jsonString
 * @return mixed
 */
-cryptoJsAesDecrypt('hhrjo40OH0XXHZ9ygwQ9','4d4a69f20c3a63ef0efe489fea7703d1b21f9eadbcfe55c9acc9cb44a335e3666ht0KGjfiM0z7ySC8J0dgg==')
+cryptoJsAesDecrypt('hhrjo40OH0XXHZ9ygwQ9',JSON.stringify('4d4a69f20c3a63ef0efe489fea7703d1b21f9eadbcfe55c9acc9cb44a335e3666ht0KGjfiM0z7ySC8J0dgg=='))
 function cryptoJsAesDecrypt($passphrase, $jsonString){
     $jsondata = json_decode($jsonString, true);
     try {
