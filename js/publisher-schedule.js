abb614c03a2df40f7aa130a9bf7c1f0f81860709
Felix <felix.gonzalez3@upr.edu>
2019-05-20 14:22:54 -0400

    publisher schedule

--- js/publisher-schedule.js
+++ js/publisher-schedule.js
@@ -1,1 +1,28 @@
 /*Publisher Schedule JavaScript*/
+
+$(document).ready(function(){
+	
+	var start = moment();  
+	
+	/**************
+	*Get EZB logos
+	***************/	
+	$.get("../server/get-image-path.php", function(data, status){
+		$("#tab-logo").attr("href", data + "img/ezb/EZBillboardsLeftLogo.png");
+        $("#ezb-logo").attr("src", data + "img/ezb/EZBillboardsLogo.png");
+	});
+
+	/************
+	*Date picker
+	************/
+  $('#reportrange').daterangepicker({
+	startDate: start,
+	minDate: start,
+	maxDate: moment().add(3,'months'),
+	singleDatePicker:true,
+	opens: 'center'
+  }, updateDate);
+
+});
+
+});
